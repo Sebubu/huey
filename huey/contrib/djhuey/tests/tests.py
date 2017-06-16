59b048fed3b7ea9b181edcf9187c82c683719ede
Severin Bühler <severin.buehler@apgsga.ch>
2017-06-16 10:02:00 +0200

    Added testing env for djhuey

---
+++ huey/contrib/djhuey/tests/tests.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class MainTest(TestCase):
+    def test_bla(self):
+        self.assertTrue(True)
--- runtests.py
+++ runtests.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python
+import os
 import sys
 import unittest
 
 from huey import tests
 
 
-def runtests(*test_args):
+def run_tests(*test_args):
     suite = unittest.TestLoader().loadTestsFromModule(tests)
     result = unittest.TextTestRunner(verbosity=1).run(suite)
     if result.failures:
         sys.exit(1)
     elif result.errors:
         sys.exit(2)
-    sys.exit(0)
+
+
+def run_django_tests(*test_args):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "huey.contrib.djhuey.tests.settings")
+    from django.core.management import execute_from_command_line
+    args = sys.argv
+    args.insert(1, "test")
+    execute_from_command_line(args)
 
 
 if __name__ == '__main__':
-    runtests(*sys.argv[1:])
+    run_django_tests(*sys.argv[1:])
+    sys.exit(0)
